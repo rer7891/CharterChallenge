ed540154f945b4ab24f59bacfa0cd9df3fc2888d
Becky Robran <rerobran@gmail.com>
2020-10-13 10:10:25 -0600

    sets up api fetch and basic restaurant list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [restaurants, setRestaurants] = useState(null);
+
+    const apiURL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
+    const apiHeader = { 
+        headers: { 
+        Authorization: 'Api-Key q3MNxtfep8Gt', 
+        }, 
+    };
+
+    const fetchData = async () => {
+      const response = await axios.get(apiURL, apiHeader)
+      .then((response) => {
+        return  setRestaurants(response.data) 
+      })
+  }
+
+useEffect (() => {
+    fetchData();
+}, [])
+
+    return (
+      <div>
+          <p>Restaraunt List</p>
+          <table> 
+              <div>
+                  {restaurants && restaurants.map(restaurant => (
+                      <tr>
+                          <td> { restaurant.name }</td>
+                          <td> { restaurant.city }</td>
+                          <td> { restaurant.state }</td>
+                          <td> { restaurant.telephone }</td>
+                          <td> { restaurant.genre }</td>
+                      </tr>
+                  
+                  ))}
+              </div>
+          </table>
+        </div>
+    );
 }
 
 export default App;
